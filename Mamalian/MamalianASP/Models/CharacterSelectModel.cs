467c638b44f4676f66da8a8fdcc05e8b1e8d8edb
ruudschouten <l12345l>
2017-05-22 12:49:34 +0200

    Updated Select so it now sends player to Play screen

---
+++ Mamalian/MamalianASP/Models/CharacterSelectModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MamalianDAL;
+using MamalianDAL.Contexts;
+using MamalianLib;
+
+namespace MamalianASP.Models
+{
+    public class CharacterSelectModel
+    {
+        public int SelectedPlayerId { get; set; }
+        public Player SelectedPlayer { get; set; }
+        public IEnumerable<SelectListItem> GetPlayers() {
+            var playerRepo = new Repository<Player>(new PlayerSQLContext());
+            var roles = playerRepo.GetAll().Select(x => new SelectListItem {
+                                    Value = x.Id.ToString(),
+                                    Text = x.Name
+                                });
+
+            return new SelectList(roles, "Value", "Text");
+        }
+    }
+}
