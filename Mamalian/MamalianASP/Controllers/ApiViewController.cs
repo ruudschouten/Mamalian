98e8bc15718606ec8e8f9e182a6bc33c5098fb3e
ruudschouten <l12345l>
2017-05-23 13:42:21 +0200

    Added instruction page for API

---
+++ Mamalian/MamalianASP/Controllers/ApiViewController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MamalianASP.Models.API;
+
+namespace MamalianASP.Controllers {
+    public class ApiViewController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
